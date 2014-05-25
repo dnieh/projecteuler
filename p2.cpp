381e97ba97b617d759315073686a259a955da30e
dnieh <danielnieh@gmail.com>
2014-05-24 22:38:55 -0700

    Move counter up
    removes the need to handle missing iteration, i.e having total equal 2.

--- p2.cpp
+++ p2.cpp
@@ -6,16 +6,15 @@ int main()
 	tempA = 1;
 	tempB = 2;
 	counter = 0;
-	total = 2;
+	total = 0;
 	
 	while (tempB <= 4000000)
 	{
+	   if (tempB % 2 == 0)
+			total += tempB;
 		counter = tempA + tempB;
 		tempA = tempB;   // a becomes 2
 		tempB = counter;  // b becomes the sum : 3
-		
-		if (counter % 2 == 0)
-			total += counter;
 	}
 	
 	std::cout << total << std::endl;
