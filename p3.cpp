423b221948859c36f98b52f8c557fbb8ea2e6d0d
dnieh <danielnieh@gmail.com>
2014-05-26 00:00:56 -0700

    add p3

---
+++ p3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int main()
+{
+	signed long long number =  600851475143;
+	long arr[32] = {0};  // Not prime factors
+	
+	// Start from x = 2 and iterate up until number % x == 0
+	long i = 3;
+	int arrCounter = 0;
+	
+	while (i <= number)
+	{
+		if (number % i == 0)
+		{
+			arr[arrCounter] = i;
+			arrCounter++;
+			number /= i;
+			
+			if (number % i == 0)
+				continue;
+				
+			else
+				i++;
+		}
+		else
+			i++;
+	}
+	
+	std::cout << arr[arrCounter - 1] << std::endl;
+			
+	return 0;
+}
